e2f2f6b910f4467240b4095c5d94b4274aeb0c60
TianLiu <tianliu@genomics.cn>
2020-08-05 09:59:18 +0800

    update
    新增说明

--- work.sh
+++ work.sh
@@ -4,7 +4,14 @@
 ### pipline for stool metagenomics assemble
 #############################################
 
-### First, modify rules/assemble.config.yaml and check the bowtie index of host genome (default: hg38)
+### First, modify rules/assemble.config.yaml and check the bowtie index of host genome (default: hg38). Replace your project_ID and queue_ID into rules/assemble.cluster.yaml.
+
+### step0 dry-run
+snakemake \
+--snakefile rules/assemble.smk \
+--configfile rules/assemble.config.yaml \
+-n
+
 ### step1.a run at test node
 snakemake \
 --snakefile rules/assemble.smk \
@@ -19,9 +26,19 @@ snakemake \
 --snakefile rules/assemble.smk \
 --configfile rules/assemble.config.yaml \
 --cluster-config rules/assemble.cluster.yaml \
---jobs 100 \
+--jobs 80 \
 --keep-going \
 --rerun-incomplete \
 --latency-wait 360 \
 --cluster "qsub -S /bin/bash -cwd -q {cluster.queue} -P {cluster.project} -l vf={cluster.mem},p={cluster.cores} -binding linear:{cluster.cores} -o {cluster.output} -e {cluster.error}"
 
+### When the project is finished, compress and archive the results.
+snakemake \
+--snakefile rules/tar.smk \
+--configfile rules/assemble.config.yaml \
+--cluster-config rules/assemble.cluster.yaml \
+--jobs 80 \
+--keep-going \
+--rerun-incomplete \
+--latency-wait 360 \
+--cluster "qsub -S /bin/bash -cwd -q {cluster.queue} -P {cluster.project} -l vf={cluster.mem},p={cluster.cores} -binding linear:{cluster.cores} -o {cluster.output} -e {cluster.error}"
