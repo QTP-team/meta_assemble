599779f9c5de00f914458d120b9fca24635b5b94
TianLiu <tianliu@genomics.cn>
2020-09-02 10:48:29 +0800

    update

--- rules/tools/dRep_format.py
+++ rules/tools/dRep_format.py
@@ -8,17 +8,17 @@ if len(sys.argv) != 3:
 else:
     infile, outfile = sys.argv[1:]
 
-dset = open(infile).readlines()[2:]
+dset = open(infile).readlines()[1:]
 
 f_o = open(outfile, "w")
 f_o.write("genome,completeness,contamination,length,N50\n")
 
 for i in dset:
     item = i.strip().split("\t")
-    genome = item[0].split("/")[-1]
-    comp = item[2]
-    cont = item[3]
-    length = item[7]
-    n50 = item[13]
+    genome = item[1].split("/")[-1]
+    comp = item[3]
+    cont = item[4]
+    length = item[8]
+    n50 = item[14]
     f_o.write("%s,%s,%s,%s,%s\n" %(genome, comp, cont, length, n50))
 f_o.close()
